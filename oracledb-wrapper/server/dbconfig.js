2b11d00e9c0e73b8130846ed792c3b735667c325
Simon Han <simon.han.mel@gmail.com>
2017-05-24 10:56:03 +1000

    Create dbconfig.js

---
+++ oracledb-wrapper/server/dbconfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    user: 'SH1202',
+    password: 'password',
+    connectString: 'localhost:1521/ID',
+    poolMax: 10,
+    poolMin: 2,
+    poolIncrement: 4,
+    poolTimeout: 4
+};
